0af5e8bf752795968b2b10ca767a234530966868
Obaid Alam <sayobaid@gmail.com>
2023-03-18 03:39:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,51 @@ import './App.css';
 import Header from './MyComponents/Header'
 import Todos from './MyComponents/Todos';
 import Footer from './MyComponents/Footer';
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import AddTodo from './MyComponents/AddTodo';
 
 
 function App() {
-  let initTodo=[]
-  if(localStorage.getItem("badtodos")!=null)
-  {
-    initTodo=JSON.parse(localStorage.getItem("badtodos"))
+  let initTodo = []
+  if (localStorage.getItem("badtodos") != null) {
+    initTodo = JSON.parse(localStorage.getItem("badtodos"))
   }
 
-  const [todosList,setTodosList]=useState(initTodo);
-  const onDelete=(todo)=> {
-  
-    console.log(todo.title +" Deleted");
-    let newTodos=todosList.filter((e)=>{
-    return e!==todo
-  });
+  const [todosList, setTodosList] = useState(initTodo);
 
-  setTodosList(newTodos)
-  localStorage.setItem("badtodos",JSON.stringify(newTodos))
+
+  const onDelete = (todo) => {
+
+    console.log(todo.title + " Deleted");
+
+    setTodosList(todosList.filter((e) => {
+      return e !== todo
+    }));
+
   }
-  const onAdd=(title,desc)=>{
-    console.log(title+" Added");
-    let newSno=todosList.length+1;
-    const newTodo={
+
+  const onAdd = (title, desc) => {
+    console.log(title + " Added");
+    let newSno = todosList.length + 1;
+    const newTodo = {
       sno: newSno,
       title: title,
       desc: desc,
-      complete:false
+      complete: false
     }
-    var newTodos=[...todosList,newTodo]
-    setTodosList(newTodos)
-    localStorage.setItem("badtodos",JSON.stringify(newTodos))
+    setTodosList([...todosList, newTodo]);
   }
 
-
+  useEffect(() => {
+    localStorage.setItem("badtodos", JSON.stringify(todosList))
+  }, [todosList]);
 
   return (
     <>
       <Header title="BadTodos" searchbar={false} logo={logo} />
-      <AddTodo addTodo={onAdd}/>
-      <Todos todos={todosList} onDelete={onDelete}/>
-     
+      <AddTodo addTodo={onAdd} />
+      <Todos todos={todosList} onDelete={onDelete} />
+
       <Footer />
     </>
   );
